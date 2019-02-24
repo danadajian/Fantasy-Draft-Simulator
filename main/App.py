e2359a236ecbf0b617abd63ef077714f8c8ac289
unknown <danadajian@gmail.com>
2019-02-24 13:10:20 -0600

    Cleaned up code thoroughly

--- main/App.py
+++ main/App.py
@@ -7,20 +7,10 @@ from tkinter.ttk import *
 from tkinter import messagebox
 
 userDraftPicks = []
-
-# ranked list of players that you want
 user_dict = {}
 
 
-# functions
-def position_ignore(player_list, pos):
-    if all(top300dict.get(player) == pos for player in player_list):
-        temp = player_list
-    else:
-        temp = [player for player in player_list if top300dict.get(player) != pos]
-    return temp
-
-
+# core functions
 def position_count(player_list, pos):
     result = [player for player in player_list if top300dict.get(player) == pos]
     return len(result)
@@ -37,6 +27,7 @@ def valid_choice(player, user_team):
 
 class draftSimulator(Tk):
 
+    # GUI
     def __init__(self, master):
         root.title('Fantasy Draft Simulator')
 
@@ -45,26 +36,30 @@ class draftSimulator(Tk):
         self.player_list_label.grid(row=0, column=0, columnspan=4, sticky=W, padx=20, pady=5)
         self.rank_list_label = Label(text='Players you want ordered by preference (drag & drop):')
         self.rank_list_label.grid(row=0, column=6, columnspan=4, sticky=W, padx=20, pady=5)
-        self.draft_count_label = Label(text='Enter number of simulations:')
-        self.draft_count_label.grid(row=11, column=0, sticky=W, padx=10, pady=10)
+        self.team_count_label = Label(text='Number of teams:')
+        self.team_count_label.grid(row=2, column=8, sticky=W, padx=5)
+        self.team_count = Entry()
+        self.team_count.grid(row=3, column=8, sticky=W, padx=5)
+        self.draft_count_label = Label(text='Number of simulations:')
+        self.draft_count_label.grid(row=4, column=8, sticky=W, padx=5)
         self.draft_count = Entry()
-        self.draft_count.grid(row=11, column=2, sticky=W, padx=10)
-        self.pick_order_label = Label(text='Which pick in the draft do you want? ')
-        self.pick_order_label.grid(row=11, column=5, sticky=W, padx=10, pady=10)
+        self.draft_count.grid(row=5, column=8, sticky=W, padx=5)
+        self.pick_order_label = Label(text='Which pick in the draft?')
+        self.pick_order_label.grid(row=6, column=8, sticky=W, padx=5)
         self.pick_order = Entry()
-        self.pick_order.grid(row=11, column=6, sticky=W, padx=10)
+        self.pick_order.grid(row=7, column=8, sticky=W, padx=5)
         self.results_list_label = Label(text='Draft Simulation Results:')
-        self.results_list_label.grid(row=13, column=0, sticky=E + W, padx=20)
+        self.results_list_label.grid(row=13, column=0, sticky=E + W, padx=20, pady=10)
 
         # lists of players and scrollbar
         self.player_list = Listbox(selectmode=MULTIPLE, activestyle='none')
-        self.player_list.grid(row=1, column=0, rowspan=10, columnspan=4, sticky=N + E + W + S, ipady=50, padx=20,
+        self.player_list.grid(row=1, column=0, rowspan=10, columnspan=5, sticky=N + E + W + S, ipadx=80, ipady=50, padx=20,
                               pady=5)
         self.user_player_list = Listbox(selectmode=SINGLE, activestyle='none')
-        self.user_player_list.grid(row=1, column=6, rowspan=10, columnspan=4, sticky=N + E + W + S, ipadx=80, ipady=50,
+        self.user_player_list.grid(row=1, column=6, rowspan=10, columnspan=2, sticky=N + E + W + S, ipadx=80, ipady=50,
                                    padx=20, pady=5)
         self.results_list = Listbox(activestyle='none', font='Monaco')
-        self.results_list.grid(row=14, column=0, rowspan=7, columnspan=9, sticky=E + W, ipady=100, padx=20, pady=10)
+        self.results_list.grid(row=14, column=0, rowspan=7, columnspan=10, sticky=E + W, ipady=100, padx=20, pady=10)
         self.left_scrollbar = Scrollbar(self.player_list, orien='vertical', command=self.player_list.yview)
         self.player_list.configure(yscrollcommand=self.left_scrollbar.set)
         self.left_scrollbar.pack(side=LEFT, fill=Y)
@@ -108,10 +103,10 @@ class draftSimulator(Tk):
         self.send_button = Button(text='Import List', command=self.import_list)
         self.send_button.grid(row=7, column=5, sticky=E + W)
         self.random_checkbox = Checkbutton(text='Random')
-        self.random_checkbox.grid(row=11, column=7, sticky=W)
+        self.random_checkbox.grid(row=7, column=9, sticky=W, padx=10)
         self.random_checkbox.state(['!alternate'])
         self.draft_button = Button(text='Draft!', command=self.simulate_draft)
-        self.draft_button.grid(row=11, column=8, sticky=W, pady=10)
+        self.draft_button.grid(row=10, column=8, sticky=E + W, pady=10)
 
         # menu
         menu = Menu(root)
@@ -124,13 +119,13 @@ class draftSimulator(Tk):
         for i in range(len(top300List)):
             self.player_list.insert(END, '       ' + str(top300List[i]) + '   ' + str(top300Positions[i]))
 
+    # functions for GUI
     def choose_players(self):
         selected_players = self.player_list.curselection()
         for i in selected_players:
             if top300List[i] not in self.user_player_list.get(0, END):
                 self.user_player_list.insert(END, str(top300List[i]))
                 user_dict.update({top300List[i]: top300Positions[i]})
-        print(user_dict)
 
     def import_list(self):
         list_to_import = (1, 2, 3, 5, 9, 8, 12, 11, 13, 15, 25, 31, 24, 42, 37, 19, 82, 43, 55, 41, 39, 60, 52, 67)
@@ -138,7 +133,6 @@ class draftSimulator(Tk):
             if top300List[i] not in self.user_player_list.get(0, END):
                 self.user_player_list.insert(END, str(top300List[i]))
                 user_dict.update({top300List[i]: top300Positions[i]})
-        print(user_dict)
 
     def remove_player(self):
         selected_players = self.user_player_list.curselection()
@@ -146,12 +140,10 @@ class draftSimulator(Tk):
         selection_list = [key for key in user_dict.keys()]
         for i in selected_players:
             del user_dict[selection_list[i]]
-        print(user_dict)
 
     def remove_all(self):
         self.user_player_list.delete(0, END)
         user_dict.clear()
-        print(user_dict)
 
     def select_all(self):
         self.player_list.select_set(first=0, last=END)
@@ -166,43 +158,48 @@ class draftSimulator(Tk):
         user_dict.clear()
         self.results_list.delete(0, END)
 
+    # draft function
     def simulate_draft(self):
         self.results_list.delete(0, END)
 
-        draft_count = int(self.draft_count.get())
-        pick_order = 0
+        try:
+            team_count = int(self.team_count.get())
+        except ValueError:
+            messagebox.showinfo('Error', 'Please specify the number of teams in your draft.')
+            return
+        if team_count < 2 or team_count > 12:
+            messagebox.showinfo('Stop', 'Number of teams must be between 2 and 12. Try again.')
+            return
+
+        try:
+            draft_count = int(self.draft_count.get())
+        except ValueError:
+            messagebox.showinfo('Error', 'Please specify the number of simulations.')
+            return
+        if draft_count > 10000:
+            messagebox.showinfo('Stop', 'Maximum of 10000 simulations allowed.')
+            return
+
         try:
             pick_order = int(self.pick_order.get())
         except ValueError:
             if self.random_checkbox.instate(['selected']):
                 pick_order = 0
             else:
-                self.random_checkbox.state(['selected'])
+                messagebox.showinfo('Error', 'Please specify your desired draft pick, or else select Random.')
+                return
+        if pick_order > team_count:
+            messagebox.showinfo('Stop', 'Pick selection exceeds number of teams.')
+            return
 
-        # dictionary and list creation
-        user_qb_list = [player for player, pos in user_dict.items() if pos == 'QB']
-        user_rb_list = [player for player, pos in user_dict.items() if pos == 'RB']
-        user_wr_list = [player for player, pos in user_dict.items() if pos == 'WR']
-        user_te_list = [player for player, pos in user_dict.items() if pos == 'TE']
-        comp_qb_list = [player for player, pos in top300dict.items() if pos == 'QB']
-        comp_rb_list = [player for player, pos in top300dict.items() if pos == 'RB']
-        comp_wr_list = [player for player, pos in top300dict.items() if pos == 'WR']
-        comp_te_list = [player for player, pos in top300dict.items() if pos == 'TE']
+        team_dict = {}
         user_team = []
-        team_2 = []
-        team_3 = []
-        team_4 = []
-        team_5 = []
-        team_6 = []
-        team_7 = []
-        team_8 = []
-
-        rounds_drafted = 0
+        for i in range(team_count - 1):
+            n = i + 2
+            team_dict['team_%s' % n] = []
+        team_dict.update({'user_team': user_team})
 
         # determine draft order if not random
-        print(self.user_player_list.get(0, END))
-        team_dict = {'user_team': user_team, 'team_2': team_2, 'team_3': team_3, 'team_4': team_4,
-                     'team_5': team_5, 'team_6': team_6, 'team_7': team_7, 'team_8': team_8}
         draft_order = [team for team in team_dict.keys()]
         if self.random_checkbox.instate(['']):
             user_pick = draft_order.index('user_team')
@@ -210,60 +207,64 @@ class draftSimulator(Tk):
                 user_pick]
         user_draft_pick = draft_order.index('user_team')
 
+        rounds_drafted = 16
+
+        # run simulation as many times as user specifies
         for _ in range(draft_count):
-            # making player lists of each position
-            user_list = [i for i in self.user_player_list.get(0, END)]
-            comp_list = [key for key in top300dict.keys()]
-            print(user_list)
-
-            # variables
-            draft_round = 0
-            threshold = 3
-
             # draft starts here
             print('The draft is live!')
 
+            # making player lists you and AI will draft from
+            user_list = [i for i in self.user_player_list.get(0, END)]
+            comp_list = [key for key in top300dict.keys()]
+
+            # reset your team
+            user_team = []
+
+            # reset team dict
+            team_dict.clear()
+            for i in range(team_count - 1):
+                n = i + 2
+                team_dict['team_%s' % n] = []
+            team_dict.update({'user_team': user_team})
+
+            # randomize draft order if selected
             if self.random_checkbox.instate(['selected']):
                 random.shuffle(draft_order)
 
-            print()
-            pick = None
+            draft_round = 0
+            threshold = 2
 
-            try:
-                while draft_round < 100:  # we only want a certain number of rounds
-                    for team in draft_order:
-                        if team == 'user_team':  # your pick logic
-                            if user_list:
-                                while not valid_choice(user_list[0], user_team):
-                                    user_list.remove(user_list[0])
-                                pick = user_list[0]
-                                user_list.remove(pick)
-                            else:
-                                comp_threshold = threshold
-                                pick = None
-                                while not valid_choice(pick, user_team):
-                                    pick = random.sample(comp_list[:threshold], 1)[0]
-                                    comp_threshold += 1
-                            user_team.append(pick)
-                            comp_list.remove(pick)
-                        else:  # AI pick logic
-                            comp_threshold = threshold
-                            while not valid_choice(pick, team_dict.get(team)):
-                                pick = random.sample(comp_list[:threshold], 1)[0]
-                                comp_threshold += 1
-                            team_dict.get(team).append(pick)
-                            if pick in user_list:
-                                user_list.remove(pick)
-                            comp_list.remove(pick)
-                    print()
-                    draft_order = draft_order[::-1]  # reverses the draft order for every other round
-                    if draft_round % 2 == 0:
-                        threshold += 2  # makes the AI choose from a larger pool of players every other round
-                    draft_round += 1  # moves on to the next round
-                    rounds_drafted += 1
-            except IndexError:
-                print("Couldn't draft all players.")
-                pass
+            while any([len(team) < rounds_drafted for team in team_dict.values()]):  # we only want 16 rounds
+                for team in draft_order:
+                    if team == 'user_team':  # your pick logic
+                        if user_list:
+                            while not valid_choice(user_list[0], user_team):
+                                user_list.remove(user_list[0])
+                            your_pick = user_list[0]
+                            user_list.remove(your_pick)
+                        else:
+                            your_threshold = threshold
+                            your_pick = None
+                            while not valid_choice(your_pick, user_team):
+                                your_pick = comp_list[random.randint(0, your_threshold)]
+                                your_threshold += 1
+                        user_team.append(your_pick)
+                        comp_list.remove(your_pick)
+                    else:  # AI pick logic
+                        comp_threshold = threshold
+                        comp_pick = None
+                        while not valid_choice(comp_pick, team_dict.get(team)):
+                            comp_pick = comp_list[random.randint(0, comp_threshold)]
+                            comp_threshold += 1
+                        team_dict.get(team).append(comp_pick)
+                        if comp_pick in user_list:
+                            user_list.remove(comp_pick)
+                        comp_list.remove(comp_pick)
+                draft_order = draft_order[::-1]  # reverses the draft order for every other round
+                if draft_round % 2 == 0:
+                    threshold += 1  # makes the AI choose from a larger pool of players every other round
+                draft_round += 1  # moves on to the next round
             print('Your Team: ' + str(user_team))
             userDraftPicks.append(user_team)
             print()
@@ -272,6 +273,7 @@ class draftSimulator(Tk):
             print('End of Draft')
             print('\n')
 
+        # aggregate simulation data for output
         user_draft_picks_final = [j for i in userDraftPicks for j in i]
         draft_frequency = {}
         for player in user_draft_picks_final:
@@ -299,22 +301,24 @@ class draftSimulator(Tk):
         self.results_list.insert(END, '-' * 55)
         self.results_list.insert(END, '    Name            Position      Draft Frequency')
         self.results_list.insert(END, '-' * 55)
+        # print results and make it look pretty
         for key, value in sorted(draft_frequency.items(), key=lambda x: x[1], reverse=True):
             key_string = key + ' ' * (22 - len(str(key)))
-            position_string = top300dict.get(key) + ' ' * 15
+            if top300dict.get(key) == 'DST':
+                position_string = top300dict.get(key) + ' ' * 14
+            elif top300dict.get(key) == 'K':
+                position_string = top300dict.get(key) + ' ' * 16
+            else:
+                position_string = top300dict.get(key) + ' ' * 15
             self.results_list.insert(END, key_string + position_string + str(round((100.0 * value), 2)) + '%')
-            # print(key_string, position_string, str(round((100.0 * value), 2)) + '%')
         self.results_list.insert(END, '\n' '\n')
         userDraftPicks.clear()
         user_draft_picks_final.clear()
         draft_frequency.clear()
         messagebox.showinfo('Nice.', 'Your drafts are complete.')
-        print('\n')
-        test_list = []
-        for i in self.user_player_list.get(0, END):
-            test_list.append(top300List.index(i))
-        test_tuple = tuple(test_list)
-        print(test_tuple)
+
+        # for adding import functionality:
+        # import_tuple = tuple([top300List.index(i) for i in self.user_player_list.get(0, END)])
 
 
 root = Tk()
--- tests/AppTests.py
+++ tests/AppTests.py
@@ -1,27 +1,13 @@
 import unittest
-from main.App import position_ignore, position_count, valid_choice
+from main.App import position_count, valid_choice
 
 
 class GetPlayersTests(unittest.TestCase):
 
-    def test_position_ignore(self):
-        test_list = ['Aaron Rodgers', 'Saquon Barkley', 'Odell Beckham Jr', 'Travis Kelce', 'Seahawks D/ST',
-                     'Justin Tucker']
-        pos_list = ['QB', 'RB', 'WR', 'TE', 'DST', 'K']
-        expected_lists = []
-        actual_lists = []
-        for pos in pos_list:
-            n = pos_list.index(pos)
-            expected_list = test_list[0:n] + test_list[n + 1:]
-            actual_list = position_ignore(test_list, pos)
-            expected_lists.append(expected_list)
-            actual_lists.append(actual_list)
-        self.assertEqual(actual_lists, expected_lists)
-
     def test_position_count(self):
         test_list = ['Saquon Barkley', 'Odell Beckham Jr', 'Julio Jones', 'Travis Kelce', 'Zach Ertz', 'George Kittle',
                      'Packers D/ST', 'Ravens D/ST', 'Jaguars D/ST', 'Seahawks D/ST', 'Justin Tucker', 'Robbie Gould',
-                     'Harrison Butker', 'Jake Elliot', 'Matt Bryant']
+                     'Harrison Butker', 'Matt Prater', 'Matt Bryant']
         pos_list = ['QB', 'RB', 'WR', 'TE', 'DST', 'K']
         expected_counts = []
         actual_counts = []
@@ -38,3 +24,4 @@ class GetPlayersTests(unittest.TestCase):
         self.assertTrue(valid_choice('Aaron Rodgers', test_team))
         self.assertTrue(valid_choice('Adam Thielen', test_team))
         self.assertFalse(valid_choice('Jaguars D/ST', test_team))
+        self.assertFalse(valid_choice(None, test_team))
